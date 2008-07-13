feafe46366fe0e73c8333f6f6726baf793f79115
sunil.ssingh <sunil.ssingh@592c7993-d951-0410-8115-35849596357c>
2008-07-13 17:52:09 +0000

    On screen text print

--- Renderer.h
+++ Renderer.h
@@ -4,6 +4,10 @@
 
 
 //------------------------------------------------------------------------------------------------------------------
+typedef std::list< std::wstring > tStringList;
+
+
+//------------------------------------------------------------------------------------------------------------------
 class cRenderer
 {
 public:
@@ -11,14 +15,18 @@ public:
 	cRenderer( IDirect3DDevice9 * pD3Ddevice, unsigned int pLineWidht );
 	~cRenderer();
 	void RenderScene( IDirect3DDevice9 * pD3DRendererDevice );
-	void RenderText( const WCHAR * pFormat, ... );
+	void PrintText( const WCHAR * pFormat, ... );
 	HRESULT CreateFont( IDirect3DDevice9 * pD3Ddevice, unsigned int pLineWidth = 15 );
-private:
 
+private:
 	//IDirect3DDevice9 * m_RendererDevice;
 	CDXUTTextHelper	*  m_TxtHelper;
     ID3DXFont*         m_Font;        // Font for drawing text
     ID3DXSprite*       m_TextSprite;  // Sprite for batching draw text calls
+	tStringList		   m_ScreenTextList;
+
+private:
+	void RenderText();
 
 protected:
 };
